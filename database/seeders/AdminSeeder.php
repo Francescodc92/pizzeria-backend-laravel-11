3068d91ac53ac49a02872280941d5fd7e8efc206
francesco <francescodicorpo1992@hotmail.com>
2024-05-14 19:11:10 +0200

    feat: add admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+      
+
+        User::create([
+            'first_name' => 'admin',
+            'last_name'=> 'admin',
+            'phone_number'=>fake()->phoneNumber(),
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ])->assignRole('admin');
+        
+    }
+}
